fc0fab63fd898f67f7108aa9ab20fb7db35d4914
super-haitam <supercoder1990@gmail.com>
2022-06-23 11:37:12 +0100

    Added Comb + Counting Sorts

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+from algo_class import SortingAlgorithm
 import random
 import pygame
-from algo_class import SortingAlgorithm 
 
 
 WIDTH, HEIGHT = 900, 500
 rand = [random.randint(0, 500) for _ in range(WIDTH)]
-# rand = [0, 7, 2, 5, 3]
+# rand = [1, 8, 0, 7, 2, 5, 0, 3, 9]
 
 sort = SortingAlgorithm()
 
@@ -13,9 +13,11 @@ algorithms_dict = {"BubbleSort": sort.BubbleSort,
     "SelectionSort": sort.SelectionSort, 
     "InsertionSort": sort.InsertionSort, 
     "CocktailShackerSort": sort.CocktailShackerSort, 
-    "OddEvenSort": sort.OddEvenSort}
-algorithms_list = ["BubbleSort", "SelectionSort", 
-    "InsertionSort", "CocktailShackerSort", "OddEvenSort"]
+    "OddEvenSort": sort.OddEvenSort,
+    "CombSort": sort.CombSort,
+    "CountingSort": sort.CountingSort}
+algorithms_list = ["BubbleSort", "SelectionSort", "InsertionSort", 
+        "CocktailShackerSort", "OddEvenSort", "CombSort", "CountingSort"]
 
 print("Choose the sorting algorithm:")
 for nb, algorithm in enumerate(algorithms_list):
@@ -26,9 +28,9 @@ choice = int(input())
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sorting Algorithms Visualizer")
 
-print(rand)
+# print(rand)
 reversed_rand = list(sorted(rand, reverse=True))
+# print(sorted(rand))
 
-sorted_rand = algorithms_dict[algorithms_list[choice]].__call__(screen, rand)
-print(rand == sorted(rand))
-print(sorted_rand == sorted(rand))
+sorted_list = algorithms_dict[algorithms_list[choice]].__call__(screen, rand)
+
